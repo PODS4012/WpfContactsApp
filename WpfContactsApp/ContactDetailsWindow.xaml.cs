87360e20a6c3ee503702072d5c7bf4a867f60fb2
D- CODE <darek_sokol@hotmail.com>
2021-03-21 23:17:19 +0100

    Add project files.

---
+++ WpfContactsApp/ContactDetailsWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfContactsApp.Models;
+
+namespace WpfContactsApp
+{
+    /// <summary>
+    /// Interaction logic for ContactDetailsWindow.xaml
+    /// </summary>
+    public partial class ContactDetailsWindow : Window
+    {
+        Contact contact;
+        public ContactDetailsWindow(Contact contact)
+        {
+            InitializeComponent();
+            Owner = Application.Current.MainWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            this.contact = contact;
+            nameTextBox.Text = contact.Name;
+            emailTextBox.Text = contact.Email;
+            phoneTextBox.Text = contact.Phone;
+        }
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            contact.Name = nameTextBox.Text;
+            contact.Email = emailTextBox.Text;
+            contact.Phone = phoneTextBox.Text;
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                MessageBoxResult dr = MessageBox.Show("Er du sikker du vil opdatere", "Opdatere data", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                if (dr == MessageBoxResult.Yes)
+                {
+                    connection.Update(contact);
+                    this.Close();
+                }
+
+            }
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                MessageBoxResult dr = MessageBox.Show("Er du sikker du vil slette", "Slet data", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                if (dr == MessageBoxResult.Yes)
+                {
+                    connection.Delete(contact);
+                    this.Close();
+                }
+                
+            }
+
+            
+        }
+    }
+}
---
+++ WpfContactsApp/NewContactWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfContactsApp.Models;
+
+namespace WpfContactsApp
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+
+            Owner = Application.Current.MainWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: Save contact
+
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text
+            };
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
+
+            //connection.Close(); - no need if using statement is applied that implements IDisposable interface on connection object
+
+            this.Close();
+        }
+
+    }
+}
